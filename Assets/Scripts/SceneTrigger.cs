bdeac491b8dee278f6fb91e48252a91140fd5b00
EAtalay8 <ekolay38@gmail.com>
2022-07-22 12:58:39 +0300

    comeback to home

--- Assets/Scripts/SceneTrigger.cs
+++ Assets/Scripts/SceneTrigger.cs
@@ -40,7 +40,12 @@ public class SceneTrigger : MonoBehaviour
 
         if (isPressE && isAvaiableForInteraction)
         {
-            SceneManager.LoadScene(1);
+            if (SceneManager.GetActiveScene () == SceneManager.GetSceneByBuildIndex (0)) 
+                SceneManager.LoadScene(1);
+
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(1))
+                SceneManager.LoadScene(0);
+
         }
     }
 
