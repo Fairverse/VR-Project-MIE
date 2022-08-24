55a660b4be3a497071390a0f157a089bf24eeed8
EAtalay8 <ekolay38@gmail.com>
2022-08-24 14:07:01 +0300

    optimizing

--- Assets/Scripts/ChosenCharacter.cs
+++ Assets/Scripts/ChosenCharacter.cs
@@ -13,35 +13,29 @@ public class ChosenCharacter : MonoBehaviour
             PlayerList.instance.SetCharacter1();
         }
 
-        if (PlayerList.instance.chosenChar == "Reyhan")
+        else if (PlayerList.instance.chosenChar == "Reyhan")
         {
             PlayerList.instance.SetCharacter2();
         }
 
-        if (PlayerList.instance.chosenChar == "Hakkican")
+        else if (PlayerList.instance.chosenChar == "Hakkican")
         {
             PlayerList.instance.SetCharacter3();
         }
 
-        if (PlayerList.instance.chosenChar == "Hadise")
+        else if (PlayerList.instance.chosenChar == "Hadise")
         {
             PlayerList.instance.SetCharacter4();
         }
 
-        if (PlayerList.instance.chosenChar == "Tarkan")
+        else if (PlayerList.instance.chosenChar == "Tarkan")
         {
             PlayerList.instance.SetCharacter5();
         }
 
-        if (PlayerList.instance.chosenChar == "Kivanc")
+        else if (PlayerList.instance.chosenChar == "Kivanc")
         {
             PlayerList.instance.SetCharacter6();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
